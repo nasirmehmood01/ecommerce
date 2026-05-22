5beeb937580bbb53a023b854350ed47fae827d47
Nasir Mehmood <nasirmalik1771@gmail.com>
2026-05-22 11:37:12 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const productRoutes = require("./src/modules/products/productRoutes");
 const cartRoutes = require("./src/modules/cart/cartRoutes");
 const checkoutRoutes = require("./src/modules/checkout/checkoutRoutes");
 const orderRoutes = require("./src/modules/orders/orderRoutes");
-// const { connectRedis } = require("./src/config/redis");
+const { connectRedis } = require("./src/config/redis");
 
 const app = express();
 
@@ -27,24 +27,24 @@ app.get("/health", (req, res) => {
 
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-// app.use("/products", productRoutes);
+app.use("/products", productRoutes);
 app.use("/cart", cartRoutes);
 app.use("/checkout", checkoutRoutes);
 app.use("/orders", orderRoutes);
 
 const PORT = process.env.PORT || 3000;
   
-// connectRedis()
-//   .then(() => {
-//     app.listen(PORT, "0.0.0.0", () => {
-//       console.log(`E-commerce API running on port ${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error("Failed to start server:", err.message);
-//     process.exit(1);
-//   });
+connectRedis()
+  .then(() => {
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`E-commerce API running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to start server:", err.message);
+    process.exit(1);
+  });
 
-app.listen(PORT, () => {
-  console.log(`E-commerce API running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`E-commerce API running on port ${PORT}`);
+// });
